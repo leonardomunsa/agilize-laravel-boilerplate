c3575a26b9a8e743e62d08b952aeb08af643acac
Leonardo Carneiro <leonardo.muncone@agilize.com.br>
2022-09-15 20:39:46 -0300

    Adiciona verificação para tamanho da questãõ

--- app/Packages/Exam/Service/QuestionService.php
+++ app/Packages/Exam/Service/QuestionService.php
@@ -8,6 +8,8 @@ use App\Packages\Exam\Repository\SubjectRepository;
 
 class QuestionService
 {
+    const MIN_LENGTH_QUESTION = 10;
+
     public function __construct(
         protected QuestionRepository $questionRepository,
         protected SubjectRepository $subjectRepository
@@ -17,9 +19,17 @@ class QuestionService
 
     public function enrollQuestion(string $content, string $subject): string
     {
-        $subject = $this->subjectRepository->findSubjectByName($subject);
-        $question = new Question($content, $subject);
-        $this->questionRepository->addQuestion($question);
-        return 'Question registered!';
+        if (!$this->checkIfQuestionIsEmpty($content)) {
+            $subject = $this->subjectRepository->findSubjectByName($subject);
+            $question = new Question($content, $subject);
+            $this->questionRepository->addQuestion($question);
+            return 'Question registered!';
+        }
+        return 'Please submit a real question';
+    }
+
+    public function checkIfQuestionIsEmpty(string $content): bool
+    {
+        return $content < self::MIN_LENGTH_QUESTION;
     }
 }
